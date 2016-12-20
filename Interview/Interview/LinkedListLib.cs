e35325a97491ac8b6dcc935541bb7cd8e68d0074
prathmeshgat <gat@usc.edu>
2016-12-19 22:43:50 -0800

    added Linked list code

---
+++ Interview/Interview/LinkedListLib.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int _d)
+        {
+            data = _d;
+        }
+    }
+
+    public class LinkedListLib
+    {
+        private Node head;
+
+        public LinkedListLib()
+        {
+            head = null;
+        }
+
+        public Node getNode(int _d)
+        {
+            return new Node(_d);
+        }
+
+        public Node Add(int _d)
+        {
+            Node tempNode = getNode(_d);
+
+            if (head == null)
+                head = tempNode;
+            else
+            {
+                Node pointer = head;
+                while (pointer.next != null)
+                    pointer = pointer.next;
+
+                pointer.next = tempNode;
+            }
+            return tempNode;
+        }
+
+        public void DisplayList()
+        {
+            Node pointer = head;
+            while (pointer != null)
+            {
+                Console.WriteLine(pointer.data);
+                pointer = pointer.next;
+            }
+        }
+
+        public int DeleteFirstOccurance(int _d)
+        {
+            if (head == null)
+                return Int32.MinValue;
+            if (head.data == _d)
+            {
+                head = head.next;
+                return _d;
+            }
+            else
+            {
+                Node pointer = head;
+                while (pointer != null)
+                {
+                    if (pointer.next != null && pointer.next.data == _d)
+                    {
+                        pointer.next = pointer.next.next;
+                        return _d;
+                    }
+                    pointer = pointer.next;
+                }
+            }
+            return Int32.MinValue;
+        }
+
+        public int kthElement(int k)
+        {
+            if (k < 0)
+                return int.MinValue;
+
+            if (head == null)
+                return int.MinValue;
+
+            int count = -1;
+            Node pointer1 = head;
+            Node pointer2 = head;
+
+            while (count < k && pointer2 != null)
+            {
+                pointer2 = pointer2.next;
+                count++;
+            }
+
+            if (count == k)
+            {
+                while (pointer2 != null)
+                {
+                    pointer2 = pointer2.next;
+                    pointer1 = pointer1.next;
+                }
+                return pointer1.data;
+            }
+            else
+                return int.MinValue;
+        }
+
+        public bool DeleteGivenNode(Node n)
+        {
+            if (n == null)
+                return false;
+            if (n.next == null)
+                return false;
+            Node nextNode = n.next;
+            n.data = nextNode.data;
+            n.next = nextNode.next;
+
+            return true;
+        }
+
+        public string getList()
+        {
+            if (head == null)
+                return null;
+            string data = null;
+            Node pointer = head;
+
+            while (pointer != null)
+            {
+                data = data + pointer.data;
+                pointer = pointer.next;
+            }
+
+            return data;
+        }
+
+        public bool isPalindrome()
+        {
+            string testList = getList();
+            if (string.IsNullOrEmpty(testList))
+                return false;
+            int pointer1 = 0;
+            int pointer2 = testList.Length - 1;
+
+            while (pointer1 < pointer2)
+            {
+                if (testList[pointer1] != testList[pointer2])
+                    return false;
+                pointer1++;
+                pointer2--;
+            }
+            return true;
+        }
+    }
+}
--- Interview/Interview/Program.cs
+++ Interview/Interview/Program.cs
@@ -10,12 +10,19 @@ namespace Interview
     {
         static void Main(string[] args)
         {
+            #region Dynamic Array
             //DynamicArray testArray = new DynamicArray();
             //testArray.demo();
+            #endregion
+
+            #region String Builder Demo
 
             //StringBuilderDemo teStringBuilderDemo = new StringBuilderDemo();
             //teStringBuilderDemo.demo();
 
+            #endregion
+
+            #region Unique Character String
             //string input = Console.ReadLine(); 
             //UniqueCharacterString testUniqueCharacterString = new UniqueCharacterString(input);
             //if(testUniqueCharacterString.isunique2())
@@ -25,6 +32,10 @@ namespace Interview
 
             //Console.Read();
 
+
+            #endregion
+
+            #region Permutation
             //string input1 = Console.ReadLine();
             //string input2 = Console.ReadLine();
             //StringPermutationCheck testPermutationCheck = new StringPermutationCheck(input1,input2);
@@ -35,10 +46,57 @@ namespace Interview
 
             //Console.Read();
 
-            URLify testUrLify = new URLify("b gzv        ", 7);
-            Console.WriteLine(testUrLify.modify());
+            #endregion
+
+            #region URLify
+
+            //URLify testUrLify = new URLify("b gzv        ", 7);
+            //Console.WriteLine(testUrLify.modify());
+
+            #endregion
+
+            #region LinkedList
+            LinkedListLib testLinkedListLib = new LinkedListLib();
+
+            testLinkedListLib.Add(3);
+            testLinkedListLib.Add(8);
+            Node temp = testLinkedListLib.Add(3);
+            // testLinkedListLib.Add(2);
+            //testLinkedListLib.Add(4);
+            //testLinkedListLib.Add(1);
+
+            testLinkedListLib.DisplayList();
+
+            //Console.WriteLine("*************Deleting****************");
+            //testLinkedListLib.DeleteFirstOccurance(2);
+            //testLinkedListLib.DisplayList();
+
+            //Console.WriteLine("*************kth from last****************");
+            //Console.WriteLine(testLinkedListLib.kthElement(3));
+
+            //Console.WriteLine("*************delete given****************");
+            //Console.WriteLine(testLinkedListLib.DeleteGivenNode(temp));
+            //testLinkedListLib.DisplayList();
+
+            Console.WriteLine("*************palindrome test****************");
+            Console.WriteLine(testLinkedListLib.isPalindrome());
 
             Console.Read();
+
+            //char c = Console.ReadKey().KeyChar;
+            //switch (c)
+            //{
+            //    case 'd':
+            //        Console.WriteLine("yay");
+            //        break;
+            //    default:
+            //        Console.WriteLine("fail");
+            //        break;
+            //}
+
+
+            #endregion
+
         }
     }
 }
